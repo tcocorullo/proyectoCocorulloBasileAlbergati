5f1e5ab086e394c60cadb1cf545350f891f35d91
tcocorullo <tomasc@udesa.edu.ar>
2025-11-14 17:08:08 -0300

    lo mismo que el anterior commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let formulario = document.querySelector(".buscador");
+let busqueda = document.querySelector(".inputb")
+
+formulario.addEventListener('submit', function(evento) {
+    evento.preventDefault();
+
+    if (busqueda.value == "" ) {
+        return alert("No ha buscado nada, ingrese un producto")
+    } if (busqueda.value.length < 3) {
+        return alert("El producto buscado debe tener minimo 3 caracteres")
+    } else {
+        this.submit()
+    }
+})
