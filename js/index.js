1d0ba9b2af094114fb34faf0b9f5f7bc8607c9ce
tcocorullo <tomasc@udesa.edu.ar>
2025-11-14 18:35:44 -0300

    hice el index

--- js/index.js
+++ js/index.js
@@ -12,3 +12,57 @@ formulario.addEventListener('submit', function(evento) {
         this.submit()
     }
 })
+
+const url1 = "https://dummyjson.com/products/categories"
+
+fetch(url1)
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(data) {
+        console.log(data);
+
+        let sidebar = document.querySelector(".lista_categorias")
+        for (let i = 0; i < data.length; i++) {
+            sidebar.innerHTML += `<li><a href="./category.html">${data[i].name}</a></li>`;
+        }
+
+    })
+.catch(function(error){
+    console.log("El error es: " + error);
+})
+
+const url2 = "https://dummyjson.com/products"
+
+fetch(url2)
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    
+    let productos = document.querySelector(".productos")
+    for (let i = 0; i <= 14; i++) {
+        productos.innerHTML +=
+            `<article class="producto">
+                <img src="${data.products[i].thumbnail}" alt=""/>
+                <h3>${data.products[i].title}</h3>
+                <p>${data.products[i].description}</p>
+                <p>Precio: ${data.products[i].price}</p>
+                <a href="./product.html?id=${data.products[i].id}" class="botones">Ver detalle</a>
+            </article>`
+    }
+
+    let productos1 = document.querySelector(".productos1")
+    for (let i = 15; i < data.products.length; i++) {
+        productos1.innerHTML +=
+            `<article class="producto">
+                <img src="${data.products[i].thumbnail}" alt=""/>
+                <h3>${data.products[i].title}</h3>
+                <p>${data.products[i].description}</p>
+                <p>Precio: ${data.products[i].price}</p>
+                <a href="./product.html?id=${data.products[i].id}" class="botones">Ver detalle</a>
+            </article>`
+    }
+
+    })
