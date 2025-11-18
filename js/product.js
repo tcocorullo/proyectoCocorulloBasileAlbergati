238ee7a913bfe4a4e39dc2ea871e9b850ab8a828
tcocorullo <tomasc@udesa.edu.ar>
2025-11-18 16:13:50 -0300

    Hice el punto numero 5

--- js/product.js
+++ js/product.js
@@ -12,3 +12,53 @@ formulario.addEventListener('submit', function(evento) {
         this.submit()
     }
 })
+
+const url2 = "https://dummyjson.com/products"
+
+fetch(url2)
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        
+        let queryString = location.search;
+        let queryStringObj = new URLSearchParams(queryString);
+        
+        let idProducto = queryStringObj.get("id")
+        
+        let producto = document.querySelector(".menu")
+        producto.innerHTML =
+            `<section class="Info1producto">
+                <h2>${data.products[idProducto].title}</h2>
+                <section class="Imagenproducto">
+                <img class="img_prod" src="${data.products[idProducto].thumbnail}" alt="Anteojos de sol Off White">
+            </section>
+                <h1>$${data.products[idProducto].price}</h1>
+            </section>
+
+            <section class="Info2">
+                <p><strong>Marca: </strong>${data.products[idProducto].brand}</p>
+                <p><strong>Descripcion: </strong>${data.products[idProducto].description}</p>
+                <p><strong>Stock Disponible: </strong>${data.products[idProducto].stock}</p>
+                <p><strong>Categoria: </strong>${data.products[idProducto].category}</p>
+            </section>`
+
+        let tags = document.querySelector(".menu2")
+        for (let i = 0; i < data.products[idProducto].tags.length; i++)
+            tags.innerHTML +=
+                `<li>#${data.products[idProducto].tags[i]}</li>`
+            
+        let reseñas = document.querySelector(".menu1")
+        for (let i = 0; i < data.products[idProducto].reviews.length; i++)
+            reseñas.innerHTML +=
+                `<section class="Reseñas">
+                        <p class="data rating"><strong>${data.products[idProducto].reviews[i].rating}/5 Estrellas</strong></p>
+                        <p class="data">${data.products[idProducto].reviews[i].date}</p>
+                        <p class="data">${data.products[idProducto].reviews[i].reviewerName}</p>
+                        <p class="data">${data.products[idProducto].reviews[i].comment} </p>  
+                </section>`
+    })
+.catch(function(error){
+    console.log("El error es: " + error);               //falta poner la fecha en el formato correcto y la marca
+})
