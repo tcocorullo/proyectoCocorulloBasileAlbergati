2e087acaeff899bfc19c8b2811fadd0519aaf5a7
jbasilecibello <jbasilecibello@udesa.edu.ar>
2025-11-19 14:17:32 -0300

    trabaje en el punto 6, category js y html

--- js/category.js
+++ js/category.js
@@ -13,22 +13,30 @@ formulario.addEventListener('submit', function(evento) {
     }
 })
 
-const url = "https://dummyjson.com/products/categories"
+const params = new URLSearchParams(window.location.search);
+const categoria = params.get("category");
 
-fetch(url)
-    .then(function(res) {
-        return res.json();
-    })
-    .then(function(data) {
-        console.log(data);
+fetch("https://dummyjson.com/products/category/" + categoria)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    const listaProductos = document.querySelector(".lista_productos");
+    listaProductos.innerHTML = "";
 
-        let sidebar = document.querySelector(".lista_categorias")
-        for (let i = 0; i < data.length; i++) {
-            sidebar.innerHTML += `<li><a href="./category.html">${data[i].name}</a></li>`;
-        }
+    for (let i = 0; i < data.products.length; i++) {
+      const article = `
+        <article class="producto">
+            <img src="${data.products[i].images[0]}" alt="${data.products[i].title}">
+            <h3>${data.products[i].title}</h3>
+            <p>${data.products[i].description}</p>
+            <p>Precio: USD ${data.products[i].price}</p>
+            <a href="product.html?product=${data.products[i].id}"" class="botones">Ver detalle</a>
+        </article>    
+      `
+      // TODO: REVISAR QUE EL QUERYSTRING (?product) SEA EL MISMO NOMBRE USADO EN LA PAGINA DE PRODUCT.HTML
+      listaProductos.innerHTML += article;
+    }
+  })
 
-    })
-.catch(function(error){
-    console.log("El error es: " + error);
-})
 
